2c47a09d1ef646f5641fcd22b7398ca05aaf6af8
Stelios Kousouris <stkousso@192.168.1.6>
2018-11-30 23:38:22 +0000

    created a reset setup for prod to return services to green and route as well

--- Infrastructure/bin/reset_prod.sh
+++ Infrastructure/bin/reset_prod.sh
@@ -18,3 +18,17 @@ echo "Resetting Parks Production Environment in project ${GUID}-parks-prod to Gr
 # rollout followed by a Green rollout.
 
 # To be Implemented by Student
+oc delete service mlbparks-green --ignore-not-found=true
+oc delete service mlbparks-blue --ignore-not-found=true
+
+oc delete service nationalparks-green --ignore-not-found=true
+oc delete service nationalparks-blue --ignore-not-found=true
+
+oc delete service parksmap-green --ignore-not-found=true
+oc delete service parksmap-blue --ignore-not-found=true
+
+oc process -f ../templates/prod-project/stk-parks-prod-app-backend-SVC.yaml -p=DC_NAME=mlbparks-green -l app=mlbparks |oc create -f - -n ${GUID}-parks-prod
+oc process -f ../templates/prod-project/stk-parks-prod-app-backend-SVC.yaml -p=DC_NAME=nationalparks-green -l app=nationalparks |oc create -f - -n ${GUID}-parks-prod
+oc process -f ../templates/prod-project/stk-parks-prod-app-frontend-SVC.yaml -p=DC_NAME=parksmap-green -l app=parksmap |oc create -f - -n ${GUID}-parks-prod
+
+oc patch route/parksmap -p '{"spec":{"to":{"name":"parksmap-green"}}}' -n ${GUID}-parks-prod
