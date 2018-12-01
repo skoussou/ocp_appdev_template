ecc82455f524cb11d12b6488e4057127a39db69c
Stelios Kousouris <stkousso@192.168.1.6>
2018-12-01 01:09:53 +0000

    removing again bin and template from route

--- templates/ARCHIVE/testing-mongodb-objects/export-project-contents.sh
+++
@@ -1,5 +0,0 @@
-projects=$(oc get projects --output=jsonpath={.items..metadata.name})
-for project in $projects; do
-    echo "exporting project $project"
-    oc export deploymentconfigs,routes,svc,pvc,buildconfigs,secrets,configmap --as-template=$project -n $project > $project.yaml
-done
